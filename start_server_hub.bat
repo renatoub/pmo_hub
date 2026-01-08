a4aad3d89770ad17618a1396697459b859555bd5
Renato Ubaldo Moreira e Moraes <renatoub@gmail.com>
2026-01-08 20:44:23 -0300

    resolve null format_html

--- start_server_hub.bat
+++ start_server_hub.bat
@@ -1,18 +1,5 @@
 @echo off
 cd C:\EQTONE\pmo_hub\pmo_hub_%BRANCH%
 
-@REM echo --- Fazendo Backup ---
-@REM copy pmo_hub\db.sqlite3 ..\pmo_hub.sqlite3
-@REM call uv run python .\pmo_hub\manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > ..\pmo_hub_datadump.json
-
-@REM echo --- Atualizando codigo (Git Pull) ---
-@REM :: Garante que estamos na main e baixa as novidades
-@REM git checkout main
-@REM git pull origin main
-
-@REM echo --- Aplicando Migrations ---
-@REM :: Executa o migrate antes de subir o servidor
-@REM call uv run python pmo_hub\manage.py migrate
-
 echo --- Iniciando Servidor ---
 call uv run python pmo_hub\manage.py runserver 0.0.0.0:%PORT%
