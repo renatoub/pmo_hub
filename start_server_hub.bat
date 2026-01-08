a58cdaad3cd39c2e15a31888a192f66d686ba242
Renato Ubaldo Moreira e Moraes <renatoub@gmail.com>
2026-01-08 20:33:05 -0300

    Acertando actions runners

--- start_server_hub.bat
+++ start_server_hub.bat
@@ -1,18 +1,18 @@
 @echo off
 cd C:\EQTONE\pmo_hub\pmo_hub_%BRANCH%
 
-echo --- Fazendo Backup ---
-copy pmo_hub\db.sqlite3 ..\pmo_hub.sqlite3
-call uv run python .\pmo_hub\manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > ..\pmo_hub_datadump.json
+@REM echo --- Fazendo Backup ---
+@REM copy pmo_hub\db.sqlite3 ..\pmo_hub.sqlite3
+@REM call uv run python .\pmo_hub\manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > ..\pmo_hub_datadump.json
 
-echo --- Atualizando codigo (Git Pull) ---
-:: Garante que estamos na main e baixa as novidades
-git checkout main
-git pull origin main
+@REM echo --- Atualizando codigo (Git Pull) ---
+@REM :: Garante que estamos na main e baixa as novidades
+@REM git checkout main
+@REM git pull origin main
 
-echo --- Aplicando Migrations ---
-:: Executa o migrate antes de subir o servidor
-call uv run python pmo_hub\manage.py migrate
+@REM echo --- Aplicando Migrations ---
+@REM :: Executa o migrate antes de subir o servidor
+@REM call uv run python pmo_hub\manage.py migrate
 
 echo --- Iniciando Servidor ---
 call uv run python pmo_hub\manage.py runserver 0.0.0.0:%PORT%
